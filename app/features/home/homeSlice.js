1c440a10e16cd39492e3072cc80ad34c35f1290a
Dipra <dipradas5940@gmail.com>
2023-09-11 23:34:17 +0600

    passenger

--- app/features/home/homeSlice.js
+++ app/features/home/homeSlice.js
@@ -1,18 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    adult: 1
-}
+    passenger: {
+        adult: 1,
+        child: 0,
+        infant: 0
+    }
+};
 
 const homeSlice = createSlice({
     name: "home",
     initialState,
     reducers: {
-
+        increaseAdult: (state) => {
+            state.passenger.adult += 1;
+        },
+        decreaseAdult: (state) => {
+            if (state.passenger.adult > 1) {
+                state.passenger.adult -= 1;
+            }
+        },
+        increaseChild: (state) => {
+            state.passenger.child += 1;
+        },
+        decreaseChild: (state) => {
+            if (state.passenger.child > 0) {
+                state.passenger.child -= 1;
+            }
+        },
+        increaseInfant: (state) => {
+            state.passenger.infant += 1;
+        },
+        decreaseInfant: (state) => {
+            if (state.passenger.infant > 0) {
+                state.passenger.infant -= 1;
+            }
+        }
     },
-    extraReducers: builder => {
+    extraReducers: (builder) => { }
+});
 
-    }
-})
 
+
+export const { increaseAdult, decreaseAdult, increaseChild,decreaseChild, increaseInfant,decreaseInfant} = homeSlice.actions;
 export default homeSlice.reducer;
--- app/page.js
+++ app/page.js
@@ -1,17 +1,35 @@
 'use client'
 
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decreaseAdult, decreaseChild, decreaseInfant, increaseAdult, increaseChild, increaseInfant } from './features/home/homeSlice';
 
 const page = () => {
-  const { adult } = useSelector(state => state.home);
-  return (
-      <div className='mt-10 w-52 m-auto flex justify-around gap-4 items-center'>
-        <button className='px-5 py-2 bg-purple-500 text-white rounded'>Decrease</button>
-        <h1>{adult}</h1>
-        <button className='px-5 py-2 bg-purple-500 text-white rounded'>Decrease</button>
-      </div>
-  )
-}
+  const { adult, child, infant } = useSelector(state => state.home.passenger);
+  const dispatch = useDispatch();
 
-export default page
+  return (
+    <>
+      <h1 className='text-center'>Adult</h1>
+      <div className='mt-10 w-52 m-auto flex justify-around gap-4 items-center'>
+        <button className='px-5 py-2 bg-purple-500 text-white rounded' onClick={() => dispatch(decreaseAdult())}>Decrease</button>
+        <h1>{adult}</h1>
+        <button className='px-5 py-2 bg-purple-500 text-white rounded' onClick={() => dispatch(increaseAdult())}>Increase</button>
+      </div>
+      <h1 className='text-center'>Children</h1>
+      <div className='mt-10 w-52 m-auto flex justify-around gap-4 items-center'>
+        <button className='px-5 py-2 bg-purple-500 text-white rounded' onClick={() => dispatch(decreaseChild())}>Decrease</button>
+        <h1>{child}</h1>
+        <button className='px-5 py-2 bg-purple-500 text-white rounded' onClick={() => dispatch(increaseChild())}>Increase</button>
+      </div>
+      <h1 className='text-center'>Infant</h1>
+      <div className='mt-10 w-52 m-auto flex justify-around gap-4 items-center'>
+        <button className='px-5 py-2 bg-purple-500 text-white rounded' onClick={() => dispatch(decreaseInfant())}>Decrease</button>
+        <h1>{infant}</h1>
+        <button className='px-5 py-2 bg-purple-500 text-white rounded' onClick={() => dispatch(increaseInfant())}>Increase</button>
+      </div>
+    </>
+  );
+};
+
+export default page;
